1742e32704a04c9d31b88df1e6074adf6b29ae26
Mariasophia Lacsina <fialacsina@gmail.com>
2025-01-30 18:17:41 +0000

    dfkvn

--- styles.js
+++ styles.js
@@ -2,3 +2,13 @@ document.querySelector('.valentines_card').addEventListener('mouseover', functio
     const audio = document.getElementById('loveAudio');
     audio.play();
 });
+
+const petalContainer = document.querySelector('.falling-petals');
+
+for (let i = 0; i < 50; i++) {
+    const petal = document.createElement('div');
+    petal.classList.add('petal');
+    petal.style.animationDuration = ${Math.random() * 5 + 4}s;
+    petal.style.left = ${Math.random() * 100}%;
+    petalContainer.appendChild(petal);
+}
